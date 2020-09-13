cf2d30d4774be6952cffad34da52149d29af7eb8
broop <soundwanders@gmail.com>
2020-09-13 17:09:55 -0400

    need to figure out how to make div squares added by JS expand the whole grid.

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 Etch N Sketch Project
-
-https://mzl.la/3kjuCu5 // CSS grid concepts
-https://css-tricks.com/snippets/css/complete-guide-grid/ // CSS grid guide
--- assets/etchsketchscript.js
+++ assets/etchsketchscript.js
@@ -1,20 +1,20 @@
 //Etchsketchscript.js
-// use javascript to generate divs (divs=grid squares) to fill grid, instead of copy/pasting HTML to creat divs
-
-addGrid(16, 50); // addGrid 16 cols 50 rows
+// use javascript to generate divs (divs=grid squares here) to fill grid, instead of copy/pasting HTML to creat divs
 
 const container = document.querySelector('.grid-container'); //get grid-Container from index.html
-function addGrid(rows, cols) {  //create a function to add squares to the grid
-    const gridBox = rows*cols;
-    for (i=0; i<gridBox; i++) {     // for statement to add grid squares until reaches 16, 50
-        const box = document.createElement('div')
-        box.className = 'div';
-        container.appendChild(box); // add div to grid container
-    }
-    container.style.gridTemplateColumns = 'repeat(${rows, cols}, 1fr)';
 
+addGrid(16); // addGrid 16 cols 50 rows
+
+function addGrid(square) {  //creating a function to add squares to the grid as new divs
+    for (i=0; i<square*square; i++) {   
+        const box = document.createElement('div');
+        box.className = 'div';
+        container.appendChild(box) // add div to grid-container
+    }
+    container.style.gridTemplateColumns = `repeat(${square}, 1fr)`;
+    
     const gridElements = document.getElementsByClassName('div');
     for (i = 0; i < (gridElements.length); i++) {
-       gridElements[i].addEventListener('mouseenter', changeColor); //when mouse passes over grid box, change box color. not implemented yet!
+       // gridElements[i].addEventListener('mouseenter', changeColor); //when mouse passes over grid box, change box color. not implemented yet!
     }
 }
