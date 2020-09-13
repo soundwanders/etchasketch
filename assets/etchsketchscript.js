038074ce9361f7be51797bc140859c6b357db942
broop <soundwanders@gmail.com>
2020-09-13 15:52:11 -0400

    got rid of unused js code

--- assets/etchsketchscript.js
+++ assets/etchsketchscript.js
@@ -1,10 +1,9 @@
-// addgrid-Container(16);
-
+//Etchsketchscript.js
 // use javascript to generate divs (divs=grid squares) to fill grid, instead of copy/pasting HTML to creat divs
-const container = document.querySelector('.grid-container'); //get grid-Container from index.html
 
 addGrid(16, 50); // addGrid 16 cols 50 rows
 
+const container = document.querySelector('.grid-container'); //get grid-Container from index.html
 function addGrid(rows, cols) {  //create a function to add squares to the grid
     const gridBox = rows*cols;
     for (i=0; i<gridBox; i++) {     // for statement to add grid squares until reaches 16, 50
@@ -19,12 +18,3 @@ function addGrid(rows, cols) {  //create a function to add squares to the grid
        gridElements[i].addEventListener('mouseenter', changeColor); //when mouse passes over grid box, change box color. not implemented yet!
     }
 }
-
- /*
-    for (r = 0; r < (gridElements.length); r++) {
-        let box = document.createElement('divGrid');
-        box.innerText = (r + 1);
-        container.appendChild(box).className = "gridBox";
-    };
-};
-*/
