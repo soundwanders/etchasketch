dce93d7bb0111c3db93022fcd0a7468f67692a46
broop <soundwanders@gmail.com>
2020-09-13 18:16:34 -0400

    added CSS style grid-row minmax, but needed to assign the min as 50em which makes me think I just added one really large row to the grid. keep digging

--- assets/etchsketchscript.js
+++ assets/etchsketchscript.js
@@ -3,7 +3,7 @@
 
 const container = document.querySelector('.grid-container'); //get grid-Container from index.html
 
-addGrid(16); // addGrid 16 cols 50 rows
+addGrid(16); // addGrid 16 cols
 
 function addGrid(square) {  //creating a function to add squares to the grid as new divs
     for (i=0; i<square*square; i++) {   
@@ -12,7 +12,7 @@ function addGrid(square) {  //creating a function to add squares to the grid as 
         container.appendChild(box) // add div to grid-container
     }
     container.style.gridTemplateColumns = `repeat(${square}, 1fr)`;
-    
+
     const gridElements = document.getElementsByClassName('div');
     for (i = 0; i < (gridElements.length); i++) {
        // gridElements[i].addEventListener('mouseenter', changeColor); //when mouse passes over grid box, change box color. not implemented yet!
