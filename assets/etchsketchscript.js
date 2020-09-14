3cb3f76111ad670d7664017ec381736bd9e29a26
broop <soundwanders@gmail.com>
2020-09-13 21:53:08 -0400

    Solution to no rows: I had not set the height of the parent div 'grid-container' in CSS stylesheet. So any attempts to manipulate the height of the gridbox were unsuccessful. set height to 70% vh which is screen height and changed width:60% to width:60% vh for continuity. Need to create an EventListener to check for mouse hover, on mouse hover over Grid change the color of the grid boxes.

--- assets/etchsketchscript.js
+++ assets/etchsketchscript.js
@@ -1,20 +1,22 @@
 //Etchsketchscript.js
 // use javascript to generate divs (divs=grid squares here) to fill grid, instead of copy/pasting HTML to creat divs
 
-const container = document.querySelector('.grid-container'); //get grid-Container from index.html
+const gridContainer = document.querySelector('.grid-container'); //get grid-Container from index.html
 
-addGrid(16); // addGrid 16 cols
+generateGrid(16); // addGrid 16 cols
 
-function addGrid(square) {  //creating a function to add squares to the grid as new divs
-    for (i=0; i<square*square; i++) {   
+// Generate Grid Function
+function generateGrid(square) {  //creating a function to add squares to the grid as new divs
+    for (let i=0; i<square*square; i++) {   
         const box = document.createElement('div');
         box.className = 'div';
         container.appendChild(box) // add div to grid-container
     }
     container.style.gridTemplateColumns = `repeat(${square}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${square}, 1fr);`;
 
     const gridElements = document.getElementsByClassName('div');
-    for (i = 0; i < (gridElements.length); i++) {
-       // gridElements[i].addEventListener('mouseenter', changeColor); //when mouse passes over grid box, change box color. not implemented yet!
+    for (let i = 0; i < (gridElements.length); i++) {
+        gridElements[i].addEventListener('mouseenter', changeColor); //when mouse passes over grid box, change box color. not implemented yet!
     }
 }
