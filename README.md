cacaa6a5670e4a72f3c56dd6295346d7b4ee9934
broop <soundwanders@gmail.com>
2020-09-15 00:06:31 -0400

    grid successfully generated with div rows/cols. need to figure out how to get changeColor on mouseenter working JS.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Etch N Sketch Project
+
+
+notes
+// used javascript to generate divs (divs=grid squares here) to fill grid, instead of copy/pasting HTML to creat divs
+
+//grid rows were not generating properly. solved by implementing a default grid height of 75% of the user's screen height and styling grid-template-rows to repeat to the end of the Grid.
--- assets/etchsketchscript.js
+++ assets/etchsketchscript.js
@@ -1,22 +1,48 @@
 //Etchsketchscript.js
-// use javascript to generate divs (divs=grid squares here) to fill grid, instead of copy/pasting HTML to creat divs
 
-const gridContainer = document.querySelector('.grid-container'); //get grid-Container from index.html
+const gridContainer = document.querySelector('.grid-container'); //get grid-Container
 
-generateGrid(16); // addGrid 16 cols
+// FUNCTIONS //
 
 // Generate Grid Function
 function generateGrid(square) {  //creating a function to add squares to the grid as new divs
-    for (let i=0; i<square*square; i++) {   
+    for (let i=0; i < square*square; i++) {   
         const box = document.createElement('div');
         box.className = 'div';
-        container.appendChild(box) // add div to grid-container
+        gridContainer.appendChild(box) // add div to grid-container
     }
     container.style.gridTemplateColumns = `repeat(${square}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${square}, 1fr);`;
 
     const gridElements = document.getElementsByClassName('div');
     for (let i = 0; i < (gridElements.length); i++) {
-        gridElements[i].addEventListener('mouseenter', changeColor); //when mouse passes over grid box, change box color. not implemented yet!
+        gridElements[i].addEventListener('mouseenter', changeColor);
     }
 }
+
+//Change Color Function
+function changeColor(x) {
+    const target = x.target;
+    rgbValue1 = getRandomRGBValue();
+    rgbValue2 = getRandomRGBValue();
+    rgbValue3 = getRandomRGBValue();
+    if (target.style.backgroundColor) {
+        let newOpacity = `${Number(target.style.opacity) + 0.5}`;
+        target.style.opacity = newOpacity;
+    } else {
+        target.style.opacity = '0.5';
+        target.style.backgroundColor = `rgb(${rgbValue1}, ${rgbValue2}, ${rgbValue3})`;
+    }
+}
+
+//Get Random RGB Value Function
+function getRandomRGBValue () {
+    return Math.floor(Math.random() * (256));
+}
+
+ // addGrid 16 cols
+generateGrid(16);
+
+
+// EVENT LISTENERS //
+boom
