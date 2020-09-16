20d5bcd16712acfd9130842a71e8c1be382582f7
broop <soundwanders@gmail.com>
2020-09-16 00:20:07 -0400

    figured out how to change color on mouseover, had to target the correct div class. targeting 'x' returned error because x had not been defined. working on getting buttons to choose brush color...still broken but getting there.

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ notes
 // used javascript to generate divs (divs=grid squares here) to fill grid, instead of copy/pasting HTML to creat divs
 
 //grid rows were not generating properly. solved by implementing a default grid height of 75% of the user's screen height and styling grid-template-rows to repeat to the end of the Grid.
+
+
+//explain problem solving process for grid rows, how i approached and debugged the grid
+//explain problem solving process for mouse enter change color
--- assets/etchsketchscript.js
+++ assets/etchsketchscript.js
@@ -1,18 +1,18 @@
 //Etchsketchscript.js
 
-const gridContainer = document.querySelector('.grid-container'); //get grid-Container
+const gridContainer = document.querySelector('.grid-container'); //get div grid-Container from html
+let brushColor = "black";
 
-// FUNCTIONS //
+//-----FUNCTIONS-----//
 
-// Generate Grid Function
-function generateGrid(square) {  //creating a function to add squares to the grid as new divs
+// GENERATE GRID FUNCTION BY CREATING DIVS AND APPENDING THEM TO PARENT-GRIDCONTAINER
+function generateGrid(square) {
     for (let i=0; i < square*square; i++) {   
         const box = document.createElement('div');
         box.className = 'div';
         gridContainer.appendChild(box) // add div to grid-container
     }
-    container.style.gridTemplateColumns = `repeat(${square}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${square}, 1fr);`;
+    gridContainer.style.gridTemplateColumns = `repeat(${square}, 1fr)`;
 
     const gridElements = document.getElementsByClassName('div');
     for (let i = 0; i < (gridElements.length); i++) {
@@ -20,29 +20,80 @@ function generateGrid(square) {  //creating a function to add squares to the gri
     }
 }
 
-//Change Color Function
-function changeColor(x) {
-    const target = x.target;
+//CHANGE COLOR RANDOM RGB function
+function changeColor(square) {
+    const target = square.target;
     rgbValue1 = getRandomRGBValue();
     rgbValue2 = getRandomRGBValue();
     rgbValue3 = getRandomRGBValue();
     if (target.style.backgroundColor) {
-        let halfOpacity = `${Number(target.style.opacity) + 0.5}`;
-        target.style.opacity = halfOpacity;
-    } else {
-        target.style.opacity = '0.5';
-        target.style.backgroundColor = `rgb(${rgbValue1}, ${rgbValue2}, ${rgbValue3})`;
+        let opacity = `${Number(target.style.opacity) + 0.5}`;
     }
 }
 
-//Get Random RGB Value Function
-function getRandomRGBValue () {
+/*
+//CHANGE BRUSH COLOR USING IF, ELSE --------broken
+function changeColor(square) {
+    const target = square.target;
+    rgbValue1 = getRandomRGBValue();
+    rgbValue2 = getRandomRGBValue();
+    rgbValue3 = getRandomRGBValue();
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => box.addEventListener('mouseenter', function(box){
+        if(brushColor === "multi"){
+            let halfopacity = `${Number(target.style.opacity) + 0.5}`;
+            changeColor.target.style.opacity = halfopacity;
+            changeColor.target.style.backgroundColor = `rgb(${rgbValue1}, ${rgbValue2}, ${rgbValue3})`;
+        }
+            else if(brushColor === "black"){
+                let halfopacity = `${Number(target.style.opacity) + 0.5}`;
+                target.style.opacity = halfopacity;
+                changeColor.target.style.backgroundColor = 'black';
+            }
+            changeColor.target.style.backgroundColor = 'black';
+        }
+    ));
+};
+
+*/
+
+//GET RANDOM RGB VALUE USING 'MATH' RETURNING A RANDOM NUMBER FROM 256 RGB VALUES
+function getRandomRGBValue (){
     return Math.floor(Math.random() * (256));
 }
 
- // addGrid 16 cols
-generateGrid(16);
+//ERASE GRID BY REMOVING ALL CHILD NODES FROM DIV-GRIDCONTAINER
+function clearGrid() {
+    gridContainer.innerHTML = ('');
+    generateGrid(16);
+    alert('Grid has been reset!');
+}
 
 
-// EVENT LISTENERS //
-boom
+//-----BUTTONS-----//
+
+const buttons = document.querySelectorAll('.btn'); // Call all buttons from html
+buttons.forEach(btn => btn.addEventListener('click', function(box){ // EVENT LISTENER for user click
+    switch (square.target.id) {
+        case 'clearGrid':
+        clearGrid();
+        generateGrid(16);
+        break;
+
+        case 'black':
+            brushColor = "black";
+            clearGrid();
+            generateGrid(16);
+        break;
+
+        case 'multi':
+            brushColor = "multi";
+            clearGrid();
+            generateGrid(16);
+        break;
+    }
+}));
+
+
+ //-----ADD GRID WITH 20 COLUMNS-----//
+ generateGrid(20);
