d54af1a28292281f5a4d78f6fd46e7676db88d76
broop <soundwanders@gmail.com>
2020-09-19 14:02:48 -0400

    added eventlistener to each div box, removed onClick from html buttons and substituted it with Javascript // targeted all event divs for background style change color and opacity // got Clear Grid, Black Brush, Multicolor Brush buttons and functionality working --- was targeting javascript square instead of html div for eventlistener, so buttons were not able to understand what function to carry out. changed to eventlistener for all DIVS (the html element) and the eventlisteners were successfully attached to each grid box...TO DO - style buttons, user interface for aesthetic appeal

--- assets/etchsketchscript.js
+++ assets/etchsketchscript.js
@@ -10,54 +10,32 @@ function generateGrid(square) {
     for (let i=0; i < square*square; i++) {   
         const box = document.createElement('div');
         box.className = 'div';
-        gridContainer.appendChild(box) // add div to grid-container
+        gridContainer.appendChild(box) // add divs to grid-container
     }
     gridContainer.style.gridTemplateColumns = `repeat(${square}, 1fr)`;
 
-    const gridElements = document.getElementsByClassName('div');
-    for (let i = 0; i < (gridElements.length); i++) {
-        gridElements[i].addEventListener('mouseenter', changeColor);
-    }
-}
-
-//CHANGE COLOR RANDOM RGB function
-function changeColor(square) {
-    const target = square.target;
-    rgbValue1 = getRandomRGBValue();
-    rgbValue2 = getRandomRGBValue();
-    rgbValue3 = getRandomRGBValue();
-    if (target.style.backgroundColor) {
-        let opacity = `${Number(target.style.opacity) + 0.5}`;
-    }
-}
-
-/*
-//CHANGE BRUSH COLOR USING IF, ELSE --------broken
-function changeColor(square) {
-    const target = square.target;
-    rgbValue1 = getRandomRGBValue();
-    rgbValue2 = getRandomRGBValue();
-    rgbValue3 = getRandomRGBValue();
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach(box => box.addEventListener('mouseenter', function(box){
-        if(brushColor === "multi"){
+    const squares = document.querySelectorAll('.div');
+    //create EVENT LISTENER on mouseenter over each grid box
+    squares.forEach(box => box.addEventListener('mouseenter', function(e){
+        const target = e.target;
+        //IF BRUSH COLOR IS BLACK (DEFAULT STATE)
+        if(brushColor === "black"){
             let halfopacity = `${Number(target.style.opacity) + 0.5}`;
-            changeColor.target.style.opacity = halfopacity;
-            changeColor.target.style.backgroundColor = `rgb(${rgbValue1}, ${rgbValue2}, ${rgbValue3})`;
+            target.style.opacity = halfopacity;
+            target.style.backgroundColor = brushColor;
+            //ELSE IF BRUSH COLOR IS MULTI, USE RANDOM RGB BRUSH
+        } else if (brushColor === "multi"){
+            let halfopacity = `${Number(target.style.opacity) + 0.5}`;
+            rgbValue1 = getRandomRGBValue();
+            rgbValue2 = getRandomRGBValue();
+            rgbValue3 = getRandomRGBValue();
+            target.style.opacity = halfopacity;
+            target.style.backgroundColor = `rgb(${rgbValue1}, ${rgbValue2}, ${rgbValue3})`;
         }
-            else if(brushColor === "black"){
-                let halfopacity = `${Number(target.style.opacity) + 0.5}`;
-                target.style.opacity = halfopacity;
-                changeColor.target.style.backgroundColor = 'black';
-            }
-            changeColor.target.style.backgroundColor = 'black';
-        }
-    ));
+    }));
 };
 
-*/
-
-//GET RANDOM RGB VALUE USING 'MATH' RETURNING A RANDOM NUMBER FROM 256 RGB VALUES
+//GET RANDOM RGB VALUE USING 'MATH'
 function getRandomRGBValue (){
     return Math.floor(Math.random() * (256));
 }
@@ -65,35 +43,36 @@ function getRandomRGBValue (){
 //ERASE GRID BY REMOVING ALL CHILD NODES FROM DIV-GRIDCONTAINER
 function clearGrid() {
     gridContainer.innerHTML = ('');
-    generateGrid(16);
+    document.querySelectorAll('.box').forEach(function(s){
+    document.getElementById('gridContainer').removeChild(s);
+    })
+    generateGrid(20);
     alert('Grid has been reset!');
 }
-
 
 //-----BUTTONS-----//
 
 const buttons = document.querySelectorAll('.btn'); // Call all buttons from html
-buttons.forEach(btn => btn.addEventListener('click', function(box){ // EVENT LISTENER for user click
-    switch (square.target.id) {
+buttons.forEach(btn => btn.addEventListener('click', function(e){ // EVENT LISTENER for user click
+    switch (e.target.id) {
         case 'clearGrid':
         clearGrid();
-        generateGrid(16);
+        generateGrid(20);
         break;
 
         case 'black':
             brushColor = "black";
             clearGrid();
-            generateGrid(16);
+            generateGrid(20);
         break;
 
         case 'multi':
             brushColor = "multi";
             clearGrid();
-            generateGrid(16);
+            generateGrid(20);
         break;
     }
 }));
-
 
  //-----ADD GRID WITH 20 COLUMNS-----//
  generateGrid(20);
